ed8513950218ddaef4fcfd0b6ad2f3d6bc3a8d67
IIZODIACII <amr_x14@outlook.com>
2019-05-04 22:05:34 +0200

    Added lines & polygon clipping algorithms

---
+++ 2D Package/clipping.h
@@ -0,0 +1,191 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+    /**  Helpers   **/
+void vIntersect(int x1, int y1, int x2, int y2, int x_edge, int &x_in, int &y_in)
+{
+    x_in = x_edge;
+    y_in = y1 + (x_in-x1) * (y2 - y1) / (x2 - x1);
+}
+
+void hIntersect(int x1, int y1, int x2, int y2, int y_edge, int &x_in, int &y_in)
+{
+    y_in = y_edge;
+    x_in = x1 + (y_in-y1) /(float)((y2 - y1) / (x2 - x1));
+}
+void cpy(int newPoints[][2], int newSize,int polyPoints[][2], int &size)
+{
+    size = newSize;
+    for (int j = 0; j < size; j++)
+    {
+        polyPoints[j][0] = newPoints[j][0];
+        polyPoints[j][1] = newPoints[j][1];
+        if(j == size - 1)
+        {
+            polyPoints[j+1][0] = newPoints[0][0];
+            polyPoints[j+1][1] = newPoints[0][1];
+        }
+    }
+}
+/***************************************************/
+
+void leftClip(int polyPoints[][2], int &size, int xLeft)
+{
+    int newPoints[10][2], newSize = 0;
+    for (int i = 0; i < size; i++)
+    {
+        int k = (i+1) % size;
+        int firstPoint_X = polyPoints[i][0], firstPoint_Y = polyPoints[i][1];
+        int secondPoint_X = polyPoints[k][0], secondPoint_Y = polyPoints[k][1];
+
+        if(firstPoint_X > xLeft && secondPoint_X > xLeft)
+            {
+                newPoints[newSize][0] = secondPoint_X;
+                newPoints[newSize++][1] = secondPoint_Y;
+            }
+            else if ((firstPoint_X < xLeft && secondPoint_X > xLeft))
+            {
+                int x,y;
+                vIntersect(firstPoint_X,firstPoint_Y,secondPoint_X,secondPoint_Y,xLeft,x,y);
+
+                newPoints[newSize][0] = x;
+                newPoints[newSize++][1] = y;
+
+                newPoints[newSize][0] = secondPoint_X;
+                newPoints[newSize++][1] = secondPoint_Y;
+
+            }
+            else if ((firstPoint_X > xLeft && secondPoint_X < xLeft))
+            {
+                int x,y;
+                vIntersect(firstPoint_X,firstPoint_Y,secondPoint_X,secondPoint_Y,xLeft,x,y);
+
+                newPoints[newSize][0] = x;
+                newPoints[newSize++][1] = y;
+            }
+        }
+        cpy(newPoints,newSize,polyPoints,size);
+}
+
+void rightClip(int polyPoints[][2], int &size, int xRight)
+{
+    int newPoints[10][2], newSize = 0;
+    for (int i = 0; i < size; i++)
+    {
+        int k = (i+1) % size;
+        int firstPoint_X = polyPoints[i][0], firstPoint_Y = polyPoints[i][1];
+        int secondPoint_X = polyPoints[k][0], secondPoint_Y = polyPoints[k][1];
+
+        if(firstPoint_X < xRight && secondPoint_X < xRight)
+        {
+            newPoints[newSize][0] = secondPoint_X;
+            newPoints[newSize++][1] = secondPoint_Y;
+
+        }
+        else if (firstPoint_X > xRight && secondPoint_X < xRight)
+        {
+            int x,y;
+            vIntersect(firstPoint_X,firstPoint_Y,secondPoint_X,secondPoint_Y,xRight,x,y);
+
+            newPoints[newSize][0] = x;
+            newPoints[newSize++][1] = y;
+
+            newPoints[newSize][0] = secondPoint_X;
+            newPoints[newSize++][1] = secondPoint_Y;
+        }
+        else if ((firstPoint_X < xRight && secondPoint_X > xRight))
+        {
+            int x,y;
+            vIntersect(firstPoint_X,firstPoint_Y,secondPoint_X,secondPoint_Y,xRight,x,y);
+
+            newPoints[newSize][0] = x;
+            newPoints[newSize++][1] = y;
+        }
+    }
+    cpy(newPoints,newSize,polyPoints,size);
+}
+
+void topClip(int polyPoints[][2], int &size, int yTop)
+{
+    int newPoints[10][2], newSize = 0;
+    for (int i = 0; i < size; i++)
+    {
+        int k = (i+1) % size;
+        int firstPoint_X = polyPoints[i][0], firstPoint_Y = polyPoints[i][1];
+        int secondPoint_X = polyPoints[k][0], secondPoint_Y = polyPoints[k][1];
+
+        if(firstPoint_Y < yTop && secondPoint_Y < yTop)
+        {
+            newPoints[newSize][0] = secondPoint_X;
+            newPoints[newSize++][1] = secondPoint_Y;
+        }
+        else if (firstPoint_Y > yTop && secondPoint_Y < yTop)
+        {
+            int x,y;
+            hIntersect(firstPoint_X,firstPoint_Y,secondPoint_X,secondPoint_Y,yTop,x,y);
+
+            newPoints[newSize][0] = x;
+            newPoints[newSize++][1] = y;
+
+            newPoints[newSize][0] = secondPoint_X;
+            newPoints[newSize++][1] = secondPoint_Y;
+        }
+        else if(firstPoint_Y < yTop && secondPoint_Y > yTop)
+        {
+            int x,y;
+            hIntersect(firstPoint_X,firstPoint_Y,secondPoint_X,secondPoint_Y,yTop,x,y);
+
+            newPoints[newSize][0] = x;
+            newPoints[newSize++][1] = y;
+        }
+    }
+    cpy(newPoints,newSize,polyPoints,size);
+
+}
+
+void bottomClip(int polyPoints[][2], int &size, int yBottom)
+{
+    int newPoints[10][2], newSize = 0;
+    for (int i = 0; i < size; i++)
+    {
+        int k = (i+1) % size;
+        int firstPoint_X = polyPoints[i][0], firstPoint_Y = polyPoints[i][1];
+        int secondPoint_X = polyPoints[k][0], secondPoint_Y = polyPoints[k][1];
+
+        if(firstPoint_Y > yBottom && secondPoint_Y > yBottom)
+            {
+                newPoints[newSize][0] = secondPoint_X;
+                newPoints[newSize++][1] = secondPoint_Y;
+
+            }
+            else if ((firstPoint_Y < yBottom && secondPoint_Y > yBottom))
+            {
+                int x,y;
+                hIntersect(firstPoint_X,firstPoint_Y,secondPoint_X,secondPoint_Y,yBottom,x,y);
+
+                newPoints[newSize][0] = x;
+                newPoints[newSize++][1] = y;
+
+                newPoints[newSize][0] = secondPoint_X;
+                newPoints[newSize++][1] = secondPoint_Y;
+            }
+            else if  (firstPoint_Y > yBottom && secondPoint_Y < yBottom)
+            {
+                int x,y;
+                hIntersect(firstPoint_X,firstPoint_Y,secondPoint_X,secondPoint_Y,yBottom,x,y);
+
+                newPoints[newSize][0] = x;
+                newPoints[newSize++][1] = y;
+            }
+        }
+    cpy(newPoints,newSize,polyPoints,size);
+}
+
+void clip(int polyPoints[][2], int &size, int xLeft, int yTop, int xRight, int yBottom)
+{
+    leftClip(polyPoints,size,xLeft);
+    rightClip(polyPoints,size,xRight);
+    bottomClip(polyPoints,size,yBottom);
+    topClip(polyPoints,size,yTop);
+
+}
---
+++ 2D Package/lines.h
@@ -0,0 +1,312 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+    /**  Helpers   **/
+void swap(int &a, int &b, int &c, int &d)
+{
+    int temp = a;
+    a = b;
+    b = temp;
+
+    temp = c;
+    c = d;
+    d = temp;
+}
+/***************************************************/
+
+void lineParam(HDC hdc, int x1, int y1, int x2, int y2, COLORREF color){
+    double dx = x2 - x1, dy = y2 - y1;
+    double dt = 1.0/(max(abs(dx), abs(dy)) + 1);
+
+    for (double t = 0; t <= 1; t+= dt)
+    {
+        int x = round(x1 + t*dx);
+        int y = round(y1 + t*dy);
+        SetPixel(hdc,x,y,color);
+    }
+}
+void simpleLineDDA (HDC hdc, int x1, int y1, int x2, int y2, COLORREF color){
+     int dy = y2 - y1;
+     int dx = x2 - x1;
+
+    if (abs(dy) > abs(dx)) // if slope < 1 --> add or sub 1 from x & add or sub the slope from y
+    {
+        int yinc = dy > 0 ? 1 : -1;
+        double x = x1;
+        double xinc = (double)dx / dy * yinc;
+        for (int y = y1; y != y2; y += yinc)
+        {
+            x += xinc;
+            SetPixel(hdc, round(x), y, color);
+        }
+
+    }
+    else// if slope > 1 --> add or sub y from x & add or sub the slope from x
+    {
+        int xinc = dx > 0 ? 1 : -1;
+        double y = y1;
+        double yinc = (double)dy / dx * xinc;
+        for (int x = x1; x != x2; x += xinc)
+        {
+            SetPixel(hdc, x, round(y), color);
+            y += yinc;
+        }
+    }
+}
+void intLineDDA(HDC hdc, int x1, int y1, int x2, int y2, COLORREF color)
+{
+    int dy = y2 - y1;
+    int dx = x2 - x1;
+    double m = (double)dy / dx;
+    dy = abs(dy);
+    dx = abs(dx);
+    int x, y, e, e1 = 2 * dx, e2 = 2 * dy;
+
+    if (dx > dy) // slope is between 0 & 1 or between 0 & -1
+    {
+//        cout<<"dx = "<<dx<<" dy = "<<dy<<endl;
+
+        e = 2 * dy - dx;
+        if (x1 > x2) // to unify and make a single while loop
+            swap(x1, x2,y1, y2);
+
+        x = x1;
+        y = y1;
+        SetPixel(hdc, x, y, color);
+
+        // if  0 <= slope <= 1 we sub e1 from e if e >= 0 else add e2 and increment x,y
+        /**
+          \
+            \
+              \
+                \
+                  \
+                    \
+                      \
+                        \
+        **/
+       if (m >= 0 && m <= 1)
+        {
+//            cout << "m >= 0 && m <= 1\n";
+
+            while (x < x2)
+            {
+                x++;
+                if (e >= 0)
+                {
+                    y++;
+                    e -= e1;
+                }
+                e += e2;
+                SetPixel(hdc, x, y, color);
+            }
+        }
+        // if 0 >= slope >= -1 we add e1 from e if e >= 0 else sub e2 and increment x, decrement y
+        /**
+                              /
+                            /
+                          /
+                        /
+                      /
+                    /
+                  /
+        **/
+        else if (m <= 0 && m >= -1)
+        {
+//            cout << "m<=0 && m >= -1\n";
+            while (x < x2)
+            {
+                x++;
+                if (e <= 0)
+                {
+                    y--;
+                    e += e1;
+                }
+                e -= e2;
+
+                SetPixel(hdc, x, y, color);
+            }
+        }
+    }
+
+    else
+    {
+//        cout<<"dx = "<<dx<<" dy = "<<dy<<endl;
+
+        e = 2 * dx - dy;
+        if (y1 > y2) // to unify and make a single while loop
+            swap(x1, x2,y1, y2);
+
+        x = x1;
+        y = y1;
+        SetPixel(hdc, x, y, color);
+
+//        if slope > 1 we sub e2 from e if e >= 0 else add e1 and increment x,y
+        /**
+                  \
+                   \
+                    \
+                     \
+        **/
+        if (m > 1)
+        {
+//            cout << "m>1\n";
+            while (y < y2)
+            {
+                y++;
+                if (e >= 0)
+                {
+                    x++;
+                    e -= e2;
+                }
+                e += e1;
+
+                SetPixel(hdc, x, y, color);
+            }
+        }
+        //if slope <= -1 we add e2 from e if e >= 0 else sub e1 and increment y, decrement x
+        /**
+                        /
+                       /
+                      /
+                     /
+        **/
+        else if (m <= -1)
+        {
+//            cout << "m<=1\n";
+            x = x1;
+            y = y1;
+            while (y < y2)
+            {
+                y++;
+                if (e <= 0)
+                {
+                    x--;
+                    e += e2;
+                }
+                e -= e1;
+
+                SetPixel(hdc, x, y, color);
+            }
+        }
+    }
+}
+
+void lineMidPoint(HDC hdc, int x1, int y1, int x2, int y2, COLORREF color)
+{
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+    int x = x1;
+    int y = y1;
+    double m = (double) dy / dx;
+
+    SetPixel(hdc,x,y,color);
+
+    if (abs(dx)>abs(dy))
+    {
+        if (x1>x2)
+        {
+            swap(x1,x2);
+            swap(y1,y2);
+            dx*=-1;
+            dy*=-1;
+        }
+        x = x1;
+        y = y1;
+
+        if (m >=0 && m <=1)
+        {
+//            cout << "m >= 0 && m <= 1\n";
+            int d = dx  -2 * dy;
+            int d1 = 2*(dx - dy);
+            int d2 = -2*dy;
+            SetPixel(hdc,x,y, color);
+
+            while(x < x2)
+            {
+                x++;
+                if(d<0)
+                {
+                    d+=d1;
+                    y++;
+                }
+                else
+                    d+=d2;
+                SetPixel(hdc,x,y, color);
+            }
+        }
+        else if (m <= 0 && m >= -1)
+        {
+//            cout << "m<=0 && m >= -1\n";
+
+            int d =-1* dx -2 * dy;
+            int d1 =-2*(dx - dy);
+            int d2 = -2*dy;
+
+            while(x < x2){
+                x++;
+                if (d>0){
+                    y--;
+                    d+=d1 ;
+                }
+                else
+                    d+=d2;
+                SetPixel(hdc,x,y, color);
+            }
+        }
+    }
+    else
+    {
+        if (y1>y2)
+        {
+            swap(x1,x2);
+            swap(y1,y2);
+            dx*=-1;
+            dy*=-1;
+        }
+        x=x1;
+        y=y1;
+        if (m > 1)
+        {
+//            cout << "m>1\n";
+            int d = 2*dx - dy ;
+            int d1 = 2*(dx - dy);
+            int d2 = 2*dx;
+            while(y!=y2)
+            {
+                y++;
+                if (d>0)
+                {
+                    d+=d1;
+                    x++;
+                }
+                else
+                {
+                    d+=d2;
+                }
+                SetPixel(hdc,x,y, color);
+            }
+        }
+        else if (m <= -1)
+        {
+//            cout << "m<=1\n";
+            int d = 2*dx + dy;
+            int d1 = 2*(dx + dy);
+            int d2 = 2*dx;
+            SetPixel(hdc,x,y, color);
+
+            while(y < y2)
+            {
+                y++;
+                if (d<0){
+                    d+=d1;
+                    x--;
+                }
+                else
+                    d+=d2;
+                SetPixel(hdc,x,y, color);
+            }
+        }
+    }
+}
